de076d58e2a8ebdd4e445c2f58f4f27e1c8ce303
MindOfZak <100719443@unimail.derby.ac.uk>
2024-09-25 11:54:00 +0100

    Create hello-world.py
    hello-world week 1

---
+++ hello-world.py
@@ -0,0 +1,5 @@
+#This line prints out Hello World!
+print("Hello World is cool!")
+#This is a single-line comment (This won't run).
+#This is also a single-line comment.
+print("I am learning about comments")
