53ec53a2c826b2749bf5913c33a8e8bfecb3c2ee
MindOfZak <100719443@unimail.derby.ac.uk>
2024-09-25 12:59:05 +0100

    Finished week 1
    end of class

---
+++ main.py
@@ -0,0 +1,87 @@
+# This is a single line comment in Python
+
+# print("Replace this line with TASK 1")
+
+# print("Hello")
+# print("World")
+# print("with Comments")
+
+#class is a 'int'
+# x = type(10)
+# print(x)
+# print(type(10))
+
+#class us a 'float'
+# x = 10.3
+# print(x)
+# print(type(x))
+
+
+#class is a 'str' (string)
+# x = "this is a string"
+# print(x)
+# print(type(x))
+
+
+# x = 10 + 5              # new object 15 assigned to x
+# print(x)                # prints out 15
+# print(type(x))          # prints out <class 'int'>
+
+# x = 10 + 5 == 3 * 5     # assign the result of comparing 10 + 5 and 3 * 5. 
+# print(x)                # prints out True
+# print(type(x))          # prints out <class 'bool'>
+
+# x = 12 + 5
+# print(x)
+# print(type(x))
+# #Answer 17
+# #Class 'int'
+
+# x = 5 + 3
+# print(x)
+# print(type(x))
+# #Answer 8
+# #Class 'int'
+
+# x = 12 / 5
+# print(x)
+# print(type(x))
+# #Answer 2.4
+# #Class 'float'
+
+# x = 12 + 5 == 5 * 3.0
+# print(x)
+# print(type(x))
+# #Answer False
+# #class 'bool'
+
+# You need to fix the following lines
+# Run the code and then use the error messages to fix each line
+
+# This line has a SyntaxError
+# print("hello world")
+
+# This line has a NameError
+# print("Hello World!")
+
+# The following lines cause a TypeError
+# int1 = 100
+# str1 = "10"
+# print(int1 / int(str1))
+
+# This line is not indented properly. Fix this
+# print("Indentation matters in Python")
+
+# You will notice that we have blank lines
+# Python doesn't care about these!
+
+# There is extra whitespace inside the print statement 
+# Python doesn't care about this
+# print("Hello World!"  )
+
+# name = input("Hello, what is your name?\n")
+# age = input("What is your age?\n")
+# print(f"Hello {name}, your age is {age}.")
+
+input_string = input("Please enter something:\n")
+print(f"You entered - {input_string}")
